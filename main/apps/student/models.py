42cce7351a766c8e2541b5bf4c08dc1b0acef507
unknown <Alex@.(none)>
2012-04-09 17:29:04 -0500

    Added m2m fields for course lists

--- main/apps/student/models.py
+++ main/apps/student/models.py
@@ -6,12 +6,13 @@ from django.contrib import admin
 #from django.db.models.signals import post_save, pre_save
 #from django.utils.encoding import smart_str
 #from django.contrib.sites.models import Site
+from apps.course.models import *
 
 class Major(models.Model):
     name = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
     college = models.CharField(max_length=100)
-    
+    coursesRequired = models.ManyToManyField(Course, related_name='Required_Course');
     def __unicode__(self):
         return self.name
     
@@ -23,6 +24,9 @@ class Student(models.Model):
     id_number = models.IntegerField(default=0)
     major = models.ForeignKey(Major)
     minor = models.ForeignKey(Minor, related_name='Minor')
+    coursesWanted = models.ManyToManyField(Course, related_name='Wanted_Course');
+    coursesRegistered = models.ManyToManyField(Course, related_name='Registered_Course');
+    coursesNeeded = models.ManyToManyField(Course, related_name='Needed_Course');
     
     def __unicode__(self):
         return self.name
