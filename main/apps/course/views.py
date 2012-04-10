b84f67f409ed65f8a23a37691c97873ab5e34c31
unknown <Alex@.(none)>
2012-04-10 17:37:57 -0500

    Adding better views for courses

--- main/apps/course/views.py
+++ main/apps/course/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import logout
 from apps.college.models import *
 from django.core import serializers
 from main.views import common_args
+from apps.course.models import WEEKDAYS
 
 def course_args(request):
     args = common_args(request)
@@ -17,6 +18,7 @@ def course(request, id=None):
     if id == None:
         redirect('/courses')
     args = course_args(request)
-    args['course'] = Course.objects.get(id=id)
+    args['course'] = serializers.serialize('python', Course.objects.filter(id=id))[0]
+    args['weekdays'] = WEEKDAYS
     args.update(csrf(request))
     return render_to_response('course/course.html', args)
