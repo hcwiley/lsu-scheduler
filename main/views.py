0384aa9b29ff01e8ba57d3d05807d934bed6bf03
unknown <Alex@.(none)>
2012-05-09 21:14:10 -0500

    Should have branching of schedules

--- main/views.py
+++ main/views.py
@@ -121,50 +121,114 @@ def coursesWanted(request):
         form = CoursesWanted(request.POST)
         if form.is_valid():
             print("valid!")
-            courses = form.cleaned_data['courses']
+            courseBucket = form.cleaned_data['courses']
             student = form.cleaned_data['student_pk']
             student = Student.objects.get(id=student)
-            print courses
-            print student.name
+            #print courses
+            #print student.name
             form = CoursesWanted()
             
-            args = {'coursesWanted' : form, 'scheduledCourse': courses, 'schedule_number': '1'}
+            #this is where teh magic should happen.
+            #list of tuples (section, priority) to reference conflicts
+            allCourses = []
+            #list of courseLists to return 
+            schedules = []
+            #list of (section)s where there is no conflict
+            safeCourses = courseBucket
+            #list of tuples (section, otherSection) where there is a conflict
+            conflictingCourses = []
+            #list of low priority tossed classes
+            tossedCourses = []
+            #first pass, separate safe and conflicting courses
+            for course in courseBucket:
+                #if course.isAbstract:
+                #    safeCourses.remove(course)
+                #    for section in Course.objects.filter(title=course.title, number=course.number):
+                #        todo: iterate through these sections
+                priority = 0
+                #if the title of the course is in the courses needed, the priority is higher
+                if (student.coursesNeeded.filter(title=section.title, number=section.number) > 0):
+                    priority = 1
+                #check to make sure the time slot isn't taken
+                for otherCourse in allCourses:
+                    #if the other section's start time is the same
+                    #TODO: check time 'slot' not time start
+                    if (otherCourse[0].start_time == course.start_time ):
+                        conflictingCourses.append((course, otherCourse[0]))
+                        safeCourses.remove(course)
+                        if (safeCourses.count(otherCourse)>0):
+                            safeCourses.remove(otherCourse)
+                            
+                        #here lies unused priority code
+                        #if the other section's priority is higher
+                        #if ():
+                            #then we need to discard this class, or put it in a new schedule
+                        #    safeCourses.remove(course)
+                        #    tossedCourses.append(course)
+                        #if the other course's priority is the same
+                        #if (otherCourse[1] == priority or otherCourse[1] > priority):
+                            #then we have an impasse.  We need a new schedule
+                        #else, the other section's priority is lower, so put this one in instead
+                        #else: 
+                        #  tossedCourses.append(otherCourse[0])
+                        
+                allCourses.append((course, priority))
+
+            #second pass, adding up schedules
+            evaluateBranches(schedules, list(safeCourses), list(conflictingCourses))
+                
+            html = ""
             h = []
             for i in range(7,20):
                 h.append(i)
-            args['hours'] = h
-            args.update(csrf(request))
+            for schedule in schedules:
+                args = {'coursesWanted' : form, 'scheduledCourse': schedule, 'schedule_number': schedules.index(schedule)+1}
+                args['hours'] = h
+                args.update(csrf(request))
+                html += render('course/scheduleTable.html', args)
             
-            #this is where teh magic should happen.
-            #total courses we are trying to register 
-            wantedCourses = []
-            for course in courses:
-                wantedCourses.append([])
-                for section in Course.objects.filter(title=course.title, number=course.number):
-                    priority = 0
-                    #if the title of the course is in the courses needed, the priority is higher
-                    if (student.coursesNeeded.filter(title=section.title, number=section.number) > 0):
-                        priority = 1
-                    #check to make sure the time slot isn't taken
-                    for otherCourse in wantedCourses:
-                        #if the other section's start time is the same, and the other sections priority is higher
-                        if (otherCourse[0].start_time == section.start_time and otherCourse[1] > priority):
-                            pass
-                        elif (otherCourse[0].start_time == section.start_time and otherCourse[1] == priority):
-                            #then we have an impasse.  We need a new schedule
-                            pass
-                        else: 
-                            wantedCourses[i].append((section, priority))
-                            student.coursesWanted.append(section)
-            
-            
-            #curStudent.coursesWanted.append(courses)
-            
-            #html = render('course/scheduleTable.html', args)
-            
-            print('updated')
-            return render_to_response('course/scheduleTable.html', args)
+            return html
 
+def evaluateBranch(schedules, conflicts, blackList, branchSchedule):
+    if (len(conflicts) > 0):
+        #pop. check blackList.
+        flag1 = flag2 = False
+        conflict = conflicts.pop()
+        if (blackList.count(conflict[0]) == 0):
+            blackList.append(conflict[0])
+            flag1 = True
+            #evaluate this with conflict0
+        if (blackList.count(conflict[1]) == 0):
+            blackList.append(conflict[1])
+            flag2 = True
+            #evaluate this with conflict1
+        if (flag1):
+            path1 = list(branchSchedule)
+            path1.append(conflict[0])
+            evaluateBranch(schedules, list(conflicts), list(blackList), path1)
+        if (flag2):
+            path2 = list(branchSchedule)
+            path2.append(conflict[1])
+            evaluateBranch(schedules, list(conflicts), list(blackList), path2)
+        if (not (flag1 or flag2)):
+            evaluateBranch(schedules, list(conflicts), list(blackList), branchSchedule)
+    else:
+        #append branchSchedule to schedules
+        schedules.append(branchSchedule)
+    
+def evaluateBranches(schedules, safeCourses, conflicts):
+    if (len(conflicts) > 0):
+        conflict = conflicts.pop()
+        blackList = [conflict[0], conflict[1]]
+        path1 = list(safeCourses)
+        path1.append(conflict[0])
+        path2 = list(safeCourses)
+        path2.append(conflict[1])
+        evaluateBranch(schedules, list(conflicts), list(blackList), path1)
+        evaluateBranch(schedules, list(conflicts), list(blackList), path2)
+    else:
+        schedules.append(safeCourses)
+        
 def home(request):
     args = common_args(request)
     args['students'] = Student.objects.all()
